0b2096f8b3cac296c8809ec76cffa31b8cf3108d
liambabe <liambabe96@gmail.com>
2021-06-08 21:29:40 +1000

    basic layout for data

---
+++ CoolantTempSensorDataGenerator.h
@@ -0,0 +1,16 @@
+#ifndef CoolantTempSensorDataGenerator_h
+#define CoolantTempSensorDataGenerator_h
+
+#include "Arduino.h"
+
+class CoolantTempSensorDataGenerator
+{
+    public:
+        CoolantTempSensorDataGenerator(int Mode);
+        float GetTemp();
+    private:
+        float currentTemp;
+        void updateTemp;
+};
+
+#endif
