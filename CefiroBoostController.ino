c4eb54895cda7942c53ddfde04027fe26df3ee00
liambabe <liambabe96@gmail.com>
2021-09-05 22:26:06 +1000

    converting to bluetooth low energy

--- CefiroBoostController.ino
+++ CefiroBoostController.ino
@@ -1,12 +1,29 @@
 #include <SoftwareSerial.h>
 #include <math.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include <Arduino.h>
 
 #include "FanController.h"
 #include "TempSensorReader.h"
 #include "MapSensorReader.h"
-#include "test/CoolantTempSensorDataGenerator.h" 
+#include "src/test/CoolantTempSensorDataGenerator.h" 
 
-SoftwareSerial bluetoothSerialCon(2,3);
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true
+// SOFTWARE UART SETTINGS
+#define BLUEFRUIT_SWUART_RXD_PIN       9    // Required for software serial!
+#define BLUEFRUIT_SWUART_TXD_PIN       10   // Required for software serial!
+#define BLUEFRUIT_UART_CTS_PIN         11   // Required for software serial!
+#define BLUEFRUIT_UART_RTS_PIN         8   // Optional, set to -1 if unused
+#define BLUEFRUIT_UART_MODE_PIN        -1    // Set to -1 if unused
+
+
+
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
 
 int FanPin = 13;
 int MapPin = A0;
@@ -17,16 +34,10 @@ float mapReading = 0.0f;
 float intakeTempReading = 0.0f;
 float coolantTempReading = 0.0f;
 
-char blueToothVal; //value sent over via bluetooth
+CoolantTempSensorDataGenerator coolantTempSensorDataGenerator(0);
 
-FanController fanController(FanPin);
-TempSensorReader CoolantTempSensorReader(CoolantTempPin, 300, 1.170050316e-03, 2.792152116e-04, 0.5816839769e-07, "down");
+//#define BLUEFRUIT_HWSERIAL_NAME           Serial1
 
-struct TMapSensor {
-  MapSensorReader mapSensorReader;
-  TempSensorReader airIntakeSensorReader;
-  TMapSensor() : mapSensorReader(MapPin), airIntakeSensorReader(AirTempPin, 1000, 1.247757853e-03, 2.698625133e-04, 1.073910146e-07, "up") {}
-} tMapSensor;
 
 void setup() {
 
@@ -35,7 +46,29 @@ void setup() {
   pinMode(AirTempPin, INPUT);
   pinMode(CoolantTempPin, INPUT);
 
-  bluetoothSerialCon.begin(9600);
+while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit UriBeacon Example"));
+  Serial.println(F("------------------------------------"));
+
+/* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+  }
+  Serial.println( F("OK!") );
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(true);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+
 }
 
 void mergeData(char* mergedData) {
@@ -45,7 +78,7 @@ void mergeData(char* mergedData) {
   //char MapCharReading[6];
   //char airCharReading[6];
 
-  //dtostrf(coolantTempReading, 5, 2, coolantCharReading);
+  dtostrf(coolantTempReading, 5, 2, coolantCharReading);
   //dtostrf(mapReading, 4, 2, MapCharReading);
   //dtostrf(intakeTempReading, 5, 2, airCharReading);
 
@@ -66,19 +99,11 @@ void mergeData(char* mergedData) {
 
 void loop() {
 
-  //mapReading = tMapSensor.mapSensorReader.GetMapReading();
-  //intakeTempReading = tMapSensor.airIntakeSensorReader.GetTemp();
-  coolantTempReading = CoolantTempSensorReader.GetTemp();
-
-  if (bluetoothSerialCon.available()) { //check if data avaliable
-    blueToothVal = bluetoothSerialCon.read(); 
-  }
-
+  coolantTempReading = coolantTempSensorDataGenerator.GetTemp();
   char dataStr[24];
   mergeData(dataStr);
-  
-  bluetoothSerialCon.write(dataStr);
-  
+  ble.println(dataStr);    
+
   delay(1000);
 
 }
